60391811afe6f8a8c60ae7f88d244d2ef5aa444c
Brendan Ryan <brendan.ryan@pm.me>
2023-04-11 16:51:32 -0400

    Full CRUD operations working

---
+++ controllers/registrationsController.js
@@ -0,0 +1,96 @@
+const Registration = require('../models/RegistrationModel')
+const mongoose = require('mongoose')
+
+const getRegistrations = async (req, res) => {
+    const registrations = await Registration.find({}).sort({createdAt: -1})
+    res.status(200).json(registrations)
+}
+const getRegistration = async (req, res) => {
+    const { id } = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'That id is not valid'})
+    }
+    const registration = await Registration.findById(id)
+    if(!registration) {
+        return res.status(404).json({error: 'No such thing'})
+    }
+    res.status(200).json(registration)
+}
+
+const createRegistration = async(req, res) => {
+    const { 
+        firstName,
+        lastName,
+        email,
+        confirmEmail,
+        jobTitle,
+        companyType,
+        address,
+        city,
+        state,
+        zip,
+        country,
+        phone,
+        arrivalDate,
+        departureDate,
+        firstTime,
+        newcomers,
+        stayAtHotel
+    } = req.body
+    try {
+        const registration = await Registration.create({ 
+            firstName,
+            lastName,
+            email,
+            confirmEmail,
+            jobTitle,
+            companyType,
+            address,
+            city,
+            state,
+            zip,
+            country,
+            phone,
+            arrivalDate,
+            departureDate,
+            firstTime,
+            newcomers,
+            stayAtHotel
+        })
+        res.status(200).json(registration)       
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+const deleteRegistration = async(req, res) => {
+    const { id } = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'That id is not valid'})
+    }
+    const registration = await Registration.findOneAndDelete({_id: id})
+    if(!registration) {
+        return res.status(404).json({error: 'No such thing'})
+    }
+    res.status(200).json(registration)
+}
+const updateRegistration = async (req, res) => {
+    const { id } = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'That id is not valid'})
+    }
+    const registration = await Registration.findOneAndUpdate({_id: id}, {
+        ...req.body
+    })
+    if(!registration) {
+        return res.status(404).json({error: 'No such thing'})
+    }
+    res.status(200).json(registration)
+}
+
+module.exports = {
+    createRegistration,
+    getRegistrations,
+    getRegistration,
+    deleteRegistration,
+    updateRegistration
+}
--- routes/registrations.js
+++ routes/registrations.js
@@ -1,65 +1,20 @@
 const express = require('express')
 const Registration = require('../models/RegistrationModel')
+const { 
+    createRegistration,
+    getRegistrations,
+    getRegistration,
+    deleteRegistration,
+    updateRegistration
+ } = require('../controllers/registrationsController')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.json({mssg: 'Get all registrations'})
-})
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'Get a registration'})
-})
-router.post('/', async (req, res) => {
-    const { 
-        firstName,
-        lastName,
-        email,
-        confirmEmail,
-        jobTitle,
-        companyType,
-        address,
-        city,
-        state,
-        zip,
-        country,
-        phone,
-        arrivalDate,
-        departureDate,
-        firstTime,
-        newcomers,
-        stayAtHotel
-    } = req.body
-    try {
-        const registration = await Registration.create({ 
-            firstName,
-            lastName,
-            email,
-            confirmEmail,
-            jobTitle,
-            companyType,
-            address,
-            city,
-            state,
-            zip,
-            country,
-            phone,
-            arrivalDate,
-            departureDate,
-            firstTime,
-            newcomers,
-            stayAtHotel
-        })
-        res.status(200).json(registration)       
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-})
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'Delete a registration'})
-})
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'Update a registration'})
-})
+router.get('/', getRegistrations)
+router.get('/:id', getRegistration)
+router.post('/', createRegistration)
+router.delete('/:id', deleteRegistration)
+router.patch('/:id', updateRegistration)
 
 
 module.exports = router
