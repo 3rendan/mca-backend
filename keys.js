030d60fabdff73f1dc8c035160a3dd96fa397aa1
Brendan Ryan <brendan.ryan@pm.me>
2023-06-27 16:23:09 -0400

    Both backend and trac in the same directory

---
+++ keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  mongooseUser: process.env.MONGOOSE_USER,
+  mongooseUri: process.env.MONGOOSE_URI,
+  mongooseDatabase: process.env.MONGOOSE_DATABASE,
+  mongoosePassword: process.env.MONGOOSE_PASSWORD,
+  mongoosePort: process.env.MONGOOSE_PORT
+}
--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const MongoClient = require('mongodb').MongoClient
+const keys = require('./keys')
 const regRoutes = require('./routes/registrations')
 const stationRoutes = require('./routes/stations')
 const tracRoutes = require('./routes/tracs')
 const programRoutes = require('./routes/programs')
-const bodyParser = require("body-parser")
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
+const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true })
 
 app.use(express.json())
 app.use((req, res, next) => {
     console.log(res.path, res.method)
     next()
 })
-// app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(cors())
+app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api/registrations', regRoutes)
 app.use('/api/stations', stationRoutes)
@@ -26,14 +32,28 @@ app.get(('/'), (req, res) => {
     res.json({mssg: 'Welcome to the FM registration.'})
 })
 
-// connect to mongoDB
+// // connect to mongoDB
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log('connected to db and listening on port', process.env.PORT)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            const db = client.db('test')
+            db.collection('tracs').createIndexes([
+                {
+                    key: { _id: 1 },
+                    _id: 'id_index',
+                }
+            ])
+        })
     })
-})
-.catch((error) => {
-    console.log(error)
-})
+    .catch((error) => {
+        console.log(error)
+    })
 
+// // Redis client setup
+// const redis = require('redis')
+// const redisClient = redis.createClient({
+//     host: keys.redisHost,
+//     port: keys.redisPort,
+//     retry_strategy: () => 1000
+// })
+// const redisPublisher = redisClient.duplicate()
